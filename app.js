e18da1a02fae237631b8ad5fea45f2d546b92604
Tony DiPasquale <anthony.r.dipasquale@gmail.com>
2014-04-04 12:44:07 -0400

    Add api endpoints

--- app.js
+++ app.js
@@ -1,35 +1,35 @@
 
-/**
- * Module dependencies.
- */
-
 var express = require('express');
-var routes = require('./routes');
-var user = require('./routes/user');
 var http = require('http');
 var path = require('path');
+
+var user = require('routes/user');
+var group = require('routes/group');
 
 var app = express();
 
 // all environments
 app.set('port', process.env.PORT || 3000);
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-app.use(express.favicon());
 app.use(express.logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded());
 app.use(express.methodOverride());
 app.use(app.router);
-app.use(express.static(path.join(__dirname, 'public')));
 
 // development only
 if ('development' == app.get('env')) {
   app.use(express.errorHandler());
 }
 
-app.get('/', routes.index);
-app.get('/users', user.list);
+app.post('/user', user.create);
+app.put('/user/:id', user.update);
+
+app.get('/groups', group.read);
+app.post('/groups', group.create);
+app.put('/groups/:id/comment', group.comment);
+app.put('/groups/:id/join', group.join);
+app.put('/groups/:id/leave', group.leave);
+app.delete('/groups/:id', group.delete);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
